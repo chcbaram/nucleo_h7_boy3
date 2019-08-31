e3eb7d9801e8dcad8a7320b6603fb1760783af50
chcbaram <chcbaram@paran.com>
2019-09-01 01:21:07 +0900

    초기 프로젝트

---
+++ nucleo_h7_boy3/src/hw/hw.c
@@ -0,0 +1,31 @@
+/*
+ * hw.c
+ *
+ *  Created on: 2019. 6. 14.
+ *      Author: HanCheol Cho
+ */
+
+
+
+
+#include "hw.h"
+
+
+
+
+
+void hwInit(void)
+{
+  bspInit();
+
+  microsInit();
+  millisInit();
+  delayInit();
+  cmdifInit();
+
+  ledInit();
+  ltdcInit();
+  uartInit();
+  i2cInit();
+  uartOpen(_DEF_UART1, 57600);
+}
---
+++ nucleo_h7_boy3/src/hw/hw.h
@@ -0,0 +1,37 @@
+/*
+ * hw.h
+ *
+ *  Created on: 2019. 6. 14.
+ *      Author: HanCheol Cho
+ */
+
+#ifndef SRC_HW_HW_H_
+#define SRC_HW_HW_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "hw_def.h"
+
+
+#include "micros.h"
+#include "millis.h"
+#include "delay.h"
+#include "led.h"
+#include "uart.h"
+#include "cmdif.h"
+#include "ltdc.h"
+#include "i2c.h"
+
+
+void hwInit(void);
+
+
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif /* SRC_HW_HW_H_ */
